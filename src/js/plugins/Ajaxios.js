64b9217347c319708644794580116c14625a3519
0xkhan <0xkhan@protonmail.com>
2023-03-10 10:26:22 +0100

    Add Ajaxios a mock plugin for AJAX calls

---
+++ src/js/plugins/Ajaxios.js
@@ -0,0 +1,29 @@
+class Ajaxios {
+
+    constructor() {
+        this.timeoutSeconds = 10;
+    }
+
+    #timeout(seconds) {
+        return new Promise(function(_, reject) {
+            setTimeout(() => {
+                reject(`Request took too long! Timeout after ${seconds} seconds.`);
+            }, seconds * 1000);
+        });
+    }
+
+    async get(url) {
+        try {
+            const fetchRequest = fetch(url);
+            const response = await Promise.race([
+                fetchRequest,
+                this.#timeout(this.timeoutSeconds)
+            ]);
+            const data await response.json();
+            if (!response.ok) throw new Error(`${data.message} ${response.status}`);
+            return data;
+        } catch(err) {
+            throw err;
+        }
+    }
+}
